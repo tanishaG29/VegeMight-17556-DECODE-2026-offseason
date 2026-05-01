4c88563a78c042edec37468f93382012a65eb16a
VarshaFTC <varsha.210310@gmail.com>
2026-05-01 21:03:44 +1000

    ?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/OpMode.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.opModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -136,6 +136,4 @@ public class OpMode extends com.qualcomm.robotcore.eventloop.opmode.OpMode
     public void stop() {
     }
 
-    public static class teleOp {
-    }
 }
