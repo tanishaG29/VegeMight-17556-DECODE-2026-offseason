c2df0776ad65cdb1753232ca303ab01a28aa7cea
tanishaG29 <tganp0@eq.edu.au>
2026-05-20 20:41:11 +1000

    anddd the intake is done my guys

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/intakeSubSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/intakeSubSystem.java
@@ -1,10 +1,48 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
-
+/*
+fields (hardware + state)
+    ↓
+constructor which are the things that only happen once when the subsystem is made. eg wiring the motors, setting their run modes
+    ↓
+initialize()     ← optional, only if you want it. things you might want to reset between runs eg. timers, etc
+    ↓
+periodic()       ← always override this from SubsystemBase. things that need to happen every single loop. eg. sensor readings, telemetry, etc
+    ↓
+action methods   ← raise(), drive(), stop(), etc.
+        ↓
+getters          ← getState(), isAtTarget(), etc.
+        */
 public class intakeSubSystem extends SubsystemBase {
-    public Servo servo;
+    public MotorEx intakeMotor;
+    ElapsedTime timer;
 
+    public intakeSubSystem(HardwareMap hMap){
+        intakeMotor = hMap.get(MotorEx.class,"intakeMotor");
+        intakeMotor.setRunMode(Motor.RunMode.RawPower);
+        intakeMotor.setInverted(true);
+    }
+
+    public void initialize(){
+        timer.reset();
+    }
+
+    public void periodic(){
+        //nothign really that i can think of... please tell me if im missing something :)
+    }
+
+    public void spin(){
+        intakeMotor.set(1.0);
+    }
+
+    public void stop(){
+        intakeMotor.set(0);
+    }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/newTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/newTeleop.java
@@ -3,35 +3,47 @@ package org.firstinspires.ftc.teamcode.opModes;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.RunCommand;
+import com.arcrobotics.ftclib.command.Subsystem;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.Gamepad
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.Essentials.intakeComm;
 import org.firstinspires.ftc.teamcode.Subsystems.dtSubSystem;
+import org.firstinspires.ftc.teamcode.Subsystems.intakeSubSystem;
 
 public class newTeleop extends CommandOpMode {
     dtSubSystem driveTrain;
-    GamepadEx gamePad;
+    GamepadEx gamePad1;
+    intakeSubSystem intakeSub;
 
 
     @Override
     public void initialize() {
         driveTrain = new dtSubSystem(hardwareMap);
-        gamePad = new GamepadEx(gamepad1);
+        gamePad1 = new GamepadEx(gamepad1);
+        intakeSub = new intakeSubSystem(hardwareMap);
 
-        new RunCommand(() ->
+        //stage 3 setting default commands (just driving rn cause we only want it driving forever)
+        driveTrain.setDefaultCommand(new RunCommand(() ->
                 driveTrain.driveRobotCentric(
-                        gamePad.getLeftX(),
-                        gamePad.getLeftY(),
-                        gamePad.getRightX()
+                        gamePad1.getLeftX(),
+                        gamePad1.getLeftY(),
+                        gamePad1.getRightX()
                 ), driveTrain
-        ).schedule();
+        ));
+        schedule(driveTrain.getDefaultCommand());
         // these 3 get things automatically get how much you have pushed the joysticks and does all the calculations to give power to the motors. change direction if its opposite
         //never stops until opmode ends. RunCommand is a shortform for simple systems where it excapsulates the initalise, execute and end phases together
         //in initalize phase becasue it is like setting an alarm once but it runs multiple times.
 
-        gamePad.getGamepadButton(GamepadKeys.Button.X)
+        //section 4 which binds everything to buttons i think
+        gamePad1.getGamepadButton(GamepadKeys.Button.X)
                 .whenPressed(new InstantCommand(() -> driveTrain.resetHeading(), driveTrain));
 
+        gamePad1.getGamepadButton(GamepadKeys.Button.A).whenPressed(new intakeComm(intakeSub));
+        gamePad1.getGamepadButton(GamepadKeys.Button.RIGHT_BUMPER).whenHeld(new InstantCommand(intakeSub::spin, (Subsystem) intakeSub));
+        //i think that when this is held it will spin the axel for the intake as long as you want
     }}
