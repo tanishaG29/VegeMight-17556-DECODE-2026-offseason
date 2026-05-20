e0c0ced61b192274d0709ca452929a08e1a1ed31
tanishaG29 <tganp0@eq.edu.au>
2026-05-20 18:00:39 +1000

    YESSS FIRST SUBSYSTEM WOOHOO

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/dtSubSystem.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.arcrobotics.ftclib.command.Subsystem;
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.arcrobotics.ftclib.hardware.motors.MotorGroup;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+public class dtSubSystem extends SubsystemBase {
+    public MotorEx FL;
+    public MotorEx FR;
+    public MotorEx BL;
+    public MotorEx BR;
+    public MecanumDrive driveTrain;
+    public MotorGroup leftSideBros;
+    public MotorGroup rightSideHoes;
+    private IMU imu;
+    private boolean fieldCentric = false;
+    public dtSubSystem(HardwareMap hMap) {
+        FL = hMap.get(MotorEx.class, "FL");
+        FR = hMap.get(MotorEx.class, "FR");
+        BL = hMap.get(MotorEx.class,"BL");
+        BR = hMap.get(MotorEx.class,"BR");
+        imu = hMap.get(IMU.class,"IMU");
+        IMU.Parameters imuParameters = new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT, RevHubOrientationOnRobot.UsbFacingDirection.UP));
+            imu.initialize(imuParameters);
+            imu.resetYaw();
+
+        FL.setRunMode(MotorEx.RunMode.RawPower);
+        BL.setRunMode(MotorEx.RunMode.RawPower);
+        BR.setRunMode(MotorEx.RunMode.RawPower);
+        FR.setRunMode(MotorEx.RunMode.RawPower);
+
+        rightSideHoes = new MotorGroup(
+                FR,
+                BR
+        );
+
+        leftSideBros = new MotorGroup(
+                FL, //leader of group
+                BL
+        );
+        leftSideBros.setInverted(true);
+
+        driveTrain = new MecanumDrive(FL,BL,BR,FR);
+    }
+
+    public void initialize() {
+        imu.resetYaw();
+    }
+
+    @Override
+    public void periodic() {
+       //add anything that needs to be run on a loop like getting sensor readings
+    }
+
+    public void driveRobotCentric(double strafe, double forward, double rotate) {
+        driveTrain.driveRobotCentric(strafe, forward, rotate);
+    }
+    //this is going to be wherever the front of the robot is, it travels in that direction
+
+    public void stop() {
+        driveTrain.stop();
+    }
+
+    public void resetHeading(){
+    imu.resetYaw(); //whenever you have changed the position of the robot
+    }
+}
+
+/*
+fields (hardware + state)
+    ↓
+constructor which are the things that only happen once when the subsystem is made. eg wiring the motors, setting their run modes
+    ↓
+initialize()     ← optional, only if you want it. things you might want to reset between runs eg. timers, etc
+    ↓
+periodic()       ← always override this from SubsystemBase. things that need to happen every single loop. eg. sensor readings, telemetry, etc
+    ↓
+action methods   ← raise(), drive(), stop(), etc.
+        ↓
+getters          ← getState(), isAtTarget(), etc.
+        */
+
